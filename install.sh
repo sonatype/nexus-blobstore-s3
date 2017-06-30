3cc647c14187135ee138fe9096e7ff56a011ba7d
Andrew Taylor <ataylor@redtoad.ca>
2017-06-29 22:39:12 -0600

    Set version number to 1.0.0.

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 #!/bin/bash -e
 
-export NEXUS_VERSION=3.4.0-01
-export NEXUS_HOME=../nexus-professional-${NEXUS_VERSION}
+export S3_BLOBSTORE_VERSION=1.0.0
+export NEXUS_HOME=../nexus-professional-3.4.0-01
+# export NEXUS_HOME=../nexus-internal/target/nexus-professional-3.4.0-SNAPSHOT
 
-mkdir -p ${NEXUS_HOME}/system/org/sonatype/nexus/nexus-blobstore-s3/${NEXUS_VERSION}/
-cp target/nexus-blobstore-s3-*.jar ${NEXUS_HOME}/system/org/sonatype/nexus/nexus-blobstore-s3/${NEXUS_VERSION}/
+mkdir -p ${NEXUS_HOME}/system/org/sonatype/nexus/nexus-blobstore-s3/${S3_BLOBSTORE_VERSION}/
+cp target/nexus-blobstore-s3-*.jar ${NEXUS_HOME}/system/org/sonatype/nexus/nexus-blobstore-s3/${S3_BLOBSTORE_VERSION}/
 
-sed -i .bak  -e "/nexus-blobstore-file/a\\"$'\n'"<bundle>mvn:org.sonatype.nexus/nexus-blobstore-s3/${NEXUS_VERSION}</bundle>" ${NEXUS_HOME}/system/org/sonatype/nexus/assemblies/nexus-base-feature/*/nexus-base-feature-*-features.xml
-sed -i .bak -e "/nexus-blobstore-file/a\\"$'\n'"<bundle>mvn:org.sonatype.nexus/nexus-blobstore-s3/${NEXUS_VERSION}</bundle>" ${NEXUS_HOME}/system/org/sonatype/nexus/assemblies/nexus-core-feature/*/nexus-core-feature-*-features.xml 
+sed -i .bak  -e "/nexus-blobstore-file/a\\"$'\n'"<bundle>mvn:org.sonatype.nexus/nexus-blobstore-s3/${S3_BLOBSTORE_VERSION}</bundle>" ${NEXUS_HOME}/system/org/sonatype/nexus/assemblies/nexus-base-feature/*/nexus-base-feature-*-features.xml
+sed -i .bak -e "/nexus-blobstore-file/a\\"$'\n'"<bundle>mvn:org.sonatype.nexus/nexus-blobstore-s3/${S3_BLOBSTORE_VERSION}</bundle>" ${NEXUS_HOME}/system/org/sonatype/nexus/assemblies/nexus-core-feature/*/nexus-core-feature-*-features.xml 
