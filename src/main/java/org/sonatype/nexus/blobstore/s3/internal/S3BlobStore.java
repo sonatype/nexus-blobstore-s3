77d7c7b500a8df63fe03c57a1ddb1d59e91520cb
Andrew Taylor <ataylor@redtoad.ca>
2017-09-27 09:25:35 -0600

    Fixed regression that prevented blobstore configuration through UI.

--- src/main/java/org/sonatype/nexus/blobstore/s3/internal/S3BlobStore.java
+++ src/main/java/org/sonatype/nexus/blobstore/s3/internal/S3BlobStore.java
@@ -77,9 +77,9 @@ public class S3BlobStore
 
   public static final String BLOB_ATTRIBUTE_SUFFIX = ".properties";
 
-  public static final String CONFIG_KEY = "s3";
+  public static final String CONFIG_KEY = "file";
 
-  public static final String BUCKET_KEY = "bucket";
+  public static final String BUCKET_KEY = "path";
 
   public static final String ACCESS_KEY_ID_KEY = "accessKeyId";
 
--- src/test/java/org/sonatype/nexus/blobstore/s3/internal/S3BlobStoreTest.groovy
+++ src/test/java/org/sonatype/nexus/blobstore/s3/internal/S3BlobStoreTest.groovy
@@ -49,7 +49,7 @@ public class S3BlobStoreTest
       permanentLocationStrategy.location(_) >> { args -> args[0].toString() }
       amazonS3Factory.create(_) >> s3
       def config = new BlobStoreConfiguration()
-      config.attributes = [s3: [bucket: 'mybucket']]
+      config.attributes = [file: [path: 'mybucket']]
       def attributesS3Object = mockS3Object("""\
         |#Thu Jun 01 23:10:55 UTC 2017
         |@BlobStore.created-by=admin
